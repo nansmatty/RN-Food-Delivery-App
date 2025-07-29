0b755fa7f5efae5daa3b0747fb62b12ef4b7e600
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-07-29 19:20:25 +0530

    full setup complete

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from 'react-native';
 export default function Index() {
 	return (
 		<View className='flex-1 items-center justify-center bg-white'>
-			<Text className='text-xl font-bold text-red-500'>Welcome to Nativewind!</Text>
+			<Text className='text-5xl text-center text-primary font-quicksand-bold'>Welcome to my React Native App!</Text>
 		</View>
 	);
 }
