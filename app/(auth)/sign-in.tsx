4dc6f2c1acc57da9ff4b5b8479e2fda467aad962
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-07-30 20:07:11 +0530

    auth screens designed

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,12 +1,54 @@
-import { router } from 'expo-router';
-import React from 'react';
-import { Button, Text, View } from 'react-native';
+import CustomButton from '@/components/CustomButton';
+import CustomInput from '@/components/CustomInput';
+import { Link, router } from 'expo-router';
+import React, { useState } from 'react';
+import { Alert, Text, View } from 'react-native';
 
 const SignIn = () => {
+	const [isSubmitting, setIsSubmitting] = useState(false);
+	const [form, setForm] = useState({ email: '', password: '' });
+
+	const handleSignIn = async () => {
+		if (!form.email || !form.password) {
+			Alert.alert('Error', 'Please enter valid credentials');
+			return;
+		}
+		setIsSubmitting(true);
+		try {
+			// Call Appwrite signin functionality
+
+			Alert.alert('Success', 'User signed in successfully.');
+			router.replace('/'); // Navigate to home after successful sign-in
+		} catch (error: any) {
+			Alert.alert('Error', error.message);
+		} finally {
+			setIsSubmitting(false);
+		}
+	};
+
 	return (
-		<View>
-			<Text>SignIn</Text>
-			<Button title='Sign Up' onPress={() => router.push('/sign-up')} />
+		<View className='gap-10 bg-white rounded-lg p-5 mt-5'>
+			<CustomInput
+				placeholder='Enter your email'
+				value={form.email}
+				onChangeText={(text) => setForm((prev) => ({ ...prev, email: text }))}
+				label='Email'
+				keyboardType='email-address'
+			/>
+			<CustomInput
+				placeholder='Enter your password'
+				value={form.password}
+				onChangeText={(text) => setForm((prev) => ({ ...prev, password: text }))}
+				label='Password'
+				secureTextEntry={true}
+			/>
+			<CustomButton title='Sign In' isLoading={isSubmitting} onPress={handleSignIn} />
+			<View className='flex justify-center mt-5 flex-row gap-2'>
+				<Text className='base-regular text-gray-100'>Don't have an account?</Text>
+				<Link href='/sign-up' className='text-primary base-bold'>
+					Sign Up
+				</Link>
+			</View>
 		</View>
 	);
 };
--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,12 +1,61 @@
-import { router } from 'expo-router';
-import React from 'react';
-import { Button, Text, View } from 'react-native';
+import CustomButton from '@/components/CustomButton';
+import CustomInput from '@/components/CustomInput';
+import { Link, router } from 'expo-router';
+import React, { useState } from 'react';
+import { Alert, Text, View } from 'react-native';
 
 const SignUp = () => {
+	const [isSubmitting, setIsSubmitting] = useState(false);
+	const [form, setForm] = useState({ name: '', email: '', password: '' });
+
+	const handleSignIn = async () => {
+		if (!form.name || !form.email || !form.password) {
+			Alert.alert('Error', 'Please enter valid credentials');
+			return;
+		}
+		setIsSubmitting(true);
+		try {
+			// Call Appwrite signup functionality
+
+			Alert.alert('Success', 'User signed in successfully.');
+			router.replace('/'); // Navigate to home after successful sign-in
+		} catch (error: any) {
+			Alert.alert('Error', error.message);
+		} finally {
+			setIsSubmitting(false);
+		}
+	};
+
 	return (
-		<View>
-			<Text>SignUp</Text>
-			<Button title='Sign In' onPress={() => router.push('/sign-in')} />
+		<View className='gap-10 bg-white rounded-lg p-5 mt-5'>
+			<CustomInput
+				placeholder='Enter your name'
+				value={form.name}
+				onChangeText={(text) => setForm((prev) => ({ ...prev, name: text }))}
+				label='Name'
+			/>
+
+			<CustomInput
+				placeholder='Enter your email'
+				value={form.email}
+				onChangeText={(text) => setForm((prev) => ({ ...prev, email: text }))}
+				label='Email'
+				keyboardType='email-address'
+			/>
+			<CustomInput
+				placeholder='Enter your password'
+				value={form.password}
+				onChangeText={(text) => setForm((prev) => ({ ...prev, password: text }))}
+				label='Password'
+				secureTextEntry={true}
+			/>
+			<CustomButton title='Sign Up' isLoading={isSubmitting} onPress={handleSignIn} />
+			<View className='flex justify-center mt-5 flex-row gap-2'>
+				<Text className='base-regular text-gray-100'>Already have an account?</Text>
+				<Link href='/sign-in' className='text-primary base-bold'>
+					Sign In
+				</Link>
+			</View>
 		</View>
 	);
 };
