51cd482bde3a77a3dde304ed75f9aa51d651f602
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-08-01 14:30:49 +0530

    db architecture design in appwrite and implementing configs

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -6,7 +6,12 @@ export const appwriteConfig = {
 	projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
 	platform: process.env.EXPO_PUBLIC_APPWRITE_PLATFORM!,
 	databaseId: process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!,
+	bucketId: process.env.EXPO_PUBLIC_APPWRITE_BUCKET_ID!,
 	userCollectionId: process.env.EXPO_PUBLIC_APPWRITE_USER_COLLECTION_ID!,
+	categoryCollectionId: process.env.EXPO_PUBLIC_APPWRITE_CATEGORY_COLLECTION_ID!,
+	menuCollectionId: process.env.EXPO_PUBLIC_APPWRITE_MENU_COLLECTION_ID!,
+	customizationsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_CUSTOMIZATIONS_COLLECTION_ID!,
+	menuCustomizationsCollectionId: process.env.EXPO_PUBLIC_APPWRITE_MENU_CUSTOMIZATIONS_COLLECTION_ID!,
 };
 
 export const client = new Client();
